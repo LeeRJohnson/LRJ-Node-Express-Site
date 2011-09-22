9c666156434222e691fea4a84617752e3db6c7e9
Lee <Lee@leerjohnson.com>
2011-09-22 05:08:08 -0700

    Edited app.js via GitHub

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -35,5 +34,5 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
+app.listen(80);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
